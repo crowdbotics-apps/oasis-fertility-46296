1c2cb49978df10c1eee3f6868d9f2c0d4e81f528
Crowdbotics <team@crowdbotics.com>
2024-02-13 14:35:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "oasis-fertility-46296",
+  url: "https://oasis-fertility-46296.botics.co",
+  api: "https://oasis-fertility-46296.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
